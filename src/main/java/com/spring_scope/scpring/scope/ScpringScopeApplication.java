ed67a6d9e4ad385252e89173af30e7cb399d4f5d
alekseipeikhel <alekseipeikhel@gmail.com>
2024-06-12 13:21:46 +0300

    initial commit

---
+++ src/main/java/com/spring_scope/scpring/scope/ScpringScopeApplication.java
@@ -0,0 +1,13 @@
+package com.spring_scope.scpring.scope;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScpringScopeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScpringScopeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring_scope/scpring/scope/ScpringScopeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_scope.scpring.scope;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScpringScopeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
