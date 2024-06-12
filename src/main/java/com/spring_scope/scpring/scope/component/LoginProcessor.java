166abcf565ac345bc82e346c61488fe5e5b38516
alekseipeikhel <alekseipeikhel@gmail.com>
2024-06-12 17:01:15 +0300

    добавления LoginCountService в  сомпонент LoginProcessor

--- src/main/java/com/spring_scope/scpring/scope/component/LoginProcessor.java
+++ src/main/java/com/spring_scope/scpring/scope/component/LoginProcessor.java
@@ -1,6 +1,7 @@
 package com.spring_scope.scpring.scope.component;
 
 import com.spring_scope.scpring.scope.service.LoggedUserManegmentService;
+import com.spring_scope.scpring.scope.service.LoginCountService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -11,18 +12,22 @@ import org.springframework.web.context.annotation.RequestScope;
 public class LoginProcessor {
     // бин в области видимости сессии
     private final LoggedUserManegmentService service;
+    // бин с областью видимости для всего приложения
+    private final LoginCountService loginCountService;
     // для логирования
     private final Logger logger = LoggerFactory.getLogger(LoginProcessor.class);
 
     private String username;
     private String password;
 
-    public LoginProcessor(LoggedUserManegmentService service) {
+    public LoginProcessor(LoggedUserManegmentService service, LoginCountService loginCountService) {
         this.service = service;
+        this.loginCountService = loginCountService;
     }
 
     // метод проверки пользвателя
     public boolean login() {
+        loginCountService.increment();
         logger.info("Using instance: MyBean@" + Integer.toHexString(System.identityHashCode(this)));
         // проверяем что данные подходят
         if ("alexey".equalsIgnoreCase(username) && "pass".equalsIgnoreCase(password)) {
