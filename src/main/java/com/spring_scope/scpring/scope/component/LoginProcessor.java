290fa06a58a1788b34a422f6a508981738da0a66
alekseipeikhel <alekseipeikhel@gmail.com>
2024-06-12 15:46:15 +0300

    иньекция бина LoggedUserManagmentService в компонент

--- src/main/java/com/spring_scope/scpring/scope/component/LoginProcessor.java
+++ src/main/java/com/spring_scope/scpring/scope/component/LoginProcessor.java
@@ -1,5 +1,6 @@
 package com.spring_scope.scpring.scope.component;
 
+import com.spring_scope.scpring.scope.service.LoggedUserManegmentService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -8,17 +9,32 @@ import org.springframework.web.context.annotation.RequestScope;
 @RequestScope
 @Component
 public class LoginProcessor {
-    private Logger logger = LoggerFactory.getLogger(LoginProcessor.class);
+    // бин в области видимости сессии
+    private final LoggedUserManegmentService service;
+    // для логирования
+    private final Logger logger = LoggerFactory.getLogger(LoginProcessor.class);
+
     private String username;
     private String password;
 
-    public boolean login() {
-        logger.info("Using instance: MyBean@" + Integer.toHexString(System.identityHashCode(this)));
-        String userName = this.username;
-        String userPass = this.password;
-        return "alexey".equalsIgnoreCase(userName) && "pass".equalsIgnoreCase(userPass);
+    public LoginProcessor(LoggedUserManegmentService service) {
+        this.service = service;
     }
 
+    // метод проверки пользвателя
+    public boolean login() {
+        logger.info("Using instance: MyBean@" + Integer.toHexString(System.identityHashCode(this)));
+        // проверяем что данные подходят
+        if ("alexey".equalsIgnoreCase(username) && "pass".equalsIgnoreCase(password)) {
+            // устанавливаем для сессии значение имени и возращаем true
+            service.setUserName(username);
+            return true;
+        }
+        // иначе
+        return false;
+    }
+
+    /* getters and setters */
     public String getUsername() {
         return username;
     }
